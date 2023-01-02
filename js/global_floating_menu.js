754ea0d5ff93b776ed21a0d85ffcb6143e6e0b69
Anthony Martin <anthonymartin20@gmail.com>
2023-01-02 17:46:23 +0100

    Deploy

--- js/global_floating_menu.js
+++ js/global_floating_menu.js
@@ -1,11 +1,10 @@
 const hamburguerIcon = document.querySelector('.hamburguer-menu-icon');
 const floatingMenu = document.querySelector('.floating-menu');
-const closeIcon = document.querySelector('.floating-menu__close > img');
+const body = document.querySelector('body');
 
 hamburguerIcon.addEventListener('click', () => {
-  floatingMenu.classList.remove('hidden');
-});
-
-closeIcon.addEventListener('click', () => {
-  floatingMenu.classList.add('hidden');
+  window.scrollTo(0, 0);
+  body.classList.toggle('no-scroll');
+  hamburguerIcon.classList.toggle('active-menu');
+  floatingMenu.classList.toggle('show-menu');
 });
