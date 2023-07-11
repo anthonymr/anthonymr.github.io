c8d31dfa616b6e3988ed21983ac5bce34bdde0bd
Anthony Martin <anthonymartin20@gmail.com>
2023-07-11 09:26:41 -0400

    Add captures to projects

--- index.js
+++ index.js
@@ -3,63 +3,101 @@ const PROJECTS = [
         name: 'Ruby resorts',
         type: 'full-stack',
         description: 'Room booking system',
+        longDescription: 'Ruby Resorts project was developed using React, Redux, Ruby on Rails, and PostgreSQL. We used JWT tokens for authentication between the front and back ends. A JWT token will be generated every time a user logs in. A regular user can signup, then log in with a username and password. The user can get a list of Suites, can get details of each suite, will be able to reserve a suite, and can access their past reservations.',
         image: 'ruby-resorts.svg',
         live: 'https://ruby-resorts-front-end.onrender.com/',
         github: 'https://github.com/anthonymr/ruby-resorts-backend',
         technologies: ['Ruby on Rails', 'PostgreSQL', 'React', 'Redux'],
-        captures: ['ruby-resorts-1.png', 'ruby-resorts-2.png', 'ruby-resorts-3.png'],
-        descriptions: [
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl.',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl.',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl.'
-        ]
+        captures: [
+            {
+                src: 'ruby-resorts-1.png',
+                active: true,
+            },
+            {
+                src: 'ruby-resorts-2.png',
+                active: false,
+            },
+            {
+                src: 'ruby-resorts-3.png',
+                active: false,
+            }
+        ],
     },
     {
         name: 'BuddGet',
         type: 'full-stack',
         description: 'Budget management web app',
+        longDescription: 'BuddGet is an full-stack application to handle budgets and save some money. A regular user can create categories, add paiments and see the total spended by category. BuddGet was built using Ruby on Rails for both, front and back ends, and PostgreSQL fro data storage. BuddGet uses Devise for authentication and CanCanCan for authorization.',
         image: 'buddget.svg',
         live: 'https://buddget-yqtb.onrender.com/welcome',
         github: 'https://github.com/anthonymr/buddget',
         technologies: ['Ruby on Rails', 'PostgreSQL'],
-        captures: ['buddget-1.png', 'buddget-2.png', 'buddget-3.png'],
-        descriptions: [
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl.',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl.',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl.'
-        ]
+        captures: [
+            {
+                src: 'buddget-1.png',
+                active: true,
+            },
+            {
+                src: 'buddget-2.png',
+                active: false,
+            },
+            {
+                src: 'buddget-3.png',
+                active: false,
+            }
+        ],
     },
     {
         name: 'Stock Screener',
         type: 'front-end',
         description: 'Stock price monitoring web app',
+        longDescription: 'A mobile front-end application to live checking stock prices consuming the Financial Modeling Prep API. Stock Screener was created with JavaScript, React, and Redux following market best practices.',
         image: 'stock-screener.svg',
         live: 'https://stock-screaner.onrender.com/',
         github: 'https://github.com/anthonymr/stock-screener',
         technologies: ['JavaScript', 'CSS', 'React', 'Redux'],
-        captures: ['stock-screener-1.png', 'stock-screener-2.png', 'stock-screener-3.png'],
-        descriptions: [
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl.',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl.',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl.'
-        ]
+        captures: [
+            {
+                src: 'stock-screener-1.png',
+                active: true,
+            },
+            {
+                src: 'stock-screener-2.png',
+                active: false,
+            },
+            {
+                src: 'stock-screener-3.png',
+                active: false,
+            }
+        ],
     },
     {
         name: 'OS Conference 2025',
         type: 'front-end',
         description: 'Open source conference website',
+        longDescription: 'OS Conference 2025 is a static website for an umbrella conference, composed of a collection of events covering the most important technologies, topics, and issues affecting open source today. Created with HTML, CSS, and JavaScript. The website is fully responsive and was created using mobile-first approach.',
         image: 'os-conference.svg',
         live: 'https://anthonymr.github.io/open-source-conference-2025/',
         github: 'https://github.com/anthonymr/open-source-conference-2025',
         technologies: ['HTML', 'CSS', 'JavaScript'],
-        captures: ['os-conference-1.png', 'os-conference-2.png', 'os-conference-3.png'],
-        descriptions: [
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl.',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl.',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl aliquet nunc, sed aliquam nisl nisl vitae nisl.'
-        ]
+        captures: [
+            {
+                src: 'os-conference-1.png',
+                active: true,
+            },
+            {
+                src: 'os-conference-2.png',
+                active: false,
+            },
+            {
+                src: 'os-conference-3.png',
+                active: false,
+            }
+        ],
     }
 ]
+
+let activeProject = null;
 
 function goTo(id) {
     let element = document.getElementById(id);
@@ -94,13 +132,79 @@ function populateProjects(projects) {
         <div class="project-info">
             <h5>${project.name}</h5>
             <p class="description">${project.description}</p>
-            <p class="kind">${project.type}</p>
+            <p class="kind ${project.type}">${project.type}</p>
         </div>
         <div class="see-more"><span>see more</span></div>
         `;
 
+        projectContainer.addEventListener('click', () => {
+            openModal(project);
+        });
+
         container.appendChild(projectContainer);
     });
+}
+
+function openModal(project) {
+    const modal = document.getElementById('modal');
+    modal.classList.remove('hidden');
+    activeProject = project;
+    setCapture(project.captures);
+}
+
+function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.classList.add('hidden');
+}
+
+function setCapture(captures) {
+    captures.forEach(capture => {
+        if (capture.active) {
+            const slideDescription = document.getElementById('slide-description');
+            const slideScreen = document.getElementById('slide-screen');
+            const liveVersion = document.getElementById('live-version');
+            const githubRepo = document.getElementById('github-repo');
+            const slideTitle = document.getElementById('slide-title');
+
+            slideScreen.src = `images/captures/${capture.src}`;
+            slideScreen.alt = capture.description;
+
+            liveVersion.href = activeProject?.live;
+            githubRepo.href = activeProject?.github;
+
+            slideTitle.innerHTML = activeProject?.name;
+
+            slideDescription.innerHTML = activeProject?.longDescription;
+        }
+    });
+}
+
+function nextCapture(captures) {
+    let activeCaptureIndex = captures.findIndex(capture => capture.active);
+    captures[activeCaptureIndex].active = false;
+
+    if (activeCaptureIndex === captures.length - 1) {
+        activeCaptureIndex = 0;
+    } else {
+        activeCaptureIndex++;
+    }
+
+    captures[activeCaptureIndex].active = true;
+    setCapture(captures);
+}
+
+function previousCapture(captures) {
+    let activeCaptureIndex = captures.findIndex(capture => capture.active);
+    captures[activeCaptureIndex].active = false;
+
+    if (activeCaptureIndex === 0) {
+        activeCaptureIndex = captures.length - 1;
+    } else {
+        activeCaptureIndex--;
+    }
+
+    captures[activeCaptureIndex].active = true;
+    setCapture(captures);
 }
 
 window.onload = () => {
